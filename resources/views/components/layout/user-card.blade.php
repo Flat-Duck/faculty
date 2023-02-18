074a7f1f56c7b28b82abae4b6a20215384514328
Abdulrahman Mahidwei <a.mahidwei@outlook.com>
2023-02-18 10:26:58 +0200

    fix

--- resources/views/components/layout/user-card.blade.php
+++ resources/views/components/layout/user-card.blade.php
@@ -11,11 +11,11 @@
             </div>
         </div>
         <div class="d-flex">
-            <a href="#" class="card-btn"><!-- Download SVG icon from http://tabler-icons.io/i/mail -->
+            <a  href="{{ route('admin.members.show', ['member' => $member->id]) }}" class="card-btn">
                 <svg xmlns="http://www.w3.org/2000/svg" class="icon me-2 text-muted" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M5 4h4l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v4a2 2 0 0 1 -2 2a16 16 0 0 1 -15 -15a2 2 0 0 1 2 -2"></path></svg>
                 ارسال بريد 
             </a>
-            <a href="#" class="card-btn"><!-- Download SVG icon from http://tabler-icons.io/i/phone -->
+            <a  href="{{ route('admin.members.show', ['member' => $member->id]) }}" class="card-btn">
                 <i class="ti ti-address-book icon me-3 text-muted"></i>
                 عرض الملف
             </a>
