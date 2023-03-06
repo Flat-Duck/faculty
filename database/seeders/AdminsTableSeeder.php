548e53393a039c4b45753682616b0ba5dd4ca0d4
Abdulrahman Mahidwei <a.mahidwei@outlook.com>
2023-03-06 15:15:01 +0200

    fix admin

--- database/seeders/AdminsTableSeeder.php
+++ database/seeders/AdminsTableSeeder.php
@@ -16,6 +16,7 @@ class AdminsTableSeeder extends Seeder
     {
         \App\Models\Admin::factory()->create([
             'email' => 'admin@test.com',
+            'name' => 'يسرا خليفة خشخوش',
             'username' => 'admin',
             'is_active'=>true,
             'password' => bcrypt('123456'),
@@ -23,13 +24,15 @@ class AdminsTableSeeder extends Seeder
 
         \App\Models\Admin::factory()->create([
             'email' => 'ibraheem@test.com',
+            'name' => 'إبراهيم أبو زيد الدويبي	',
             'username' => 'ibraheem',
             'is_active'=>true,
             'password' => bcrypt('123456'),
         ]);
-            
+
         \App\Models\Admin::factory()->create([
             'email' => 'anisa@test.com',
+            'name' => 'أنيسة فتحي البخاري	',
             'username' => 'anisa',
             'is_active'=>true,
             'password' => bcrypt('123123123'),
